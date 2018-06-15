27878fc7dc072f1733690be9fdf3e9c0ca44070e
efren <carva97valvi@gmail.com>
2018-06-15 03:54:44 -0600

    Avence en Api Solicitudes

--- APIs/HorasBeca.Solicitudes.api/HorasBeca.Solicitudes.api/Models/Solicitudes.cs
+++ APIs/HorasBeca.Solicitudes.api/HorasBeca.Solicitudes.api/Models/Solicitudes.cs
@@ -6,6 +6,7 @@ using System.Web.Configuration;
 using Newtonsoft.Json.Linq;
 using System.Data.SqlClient;
 using System.Data;
+using System.Data.SqlTypes;
 
 namespace HorasBeca.Solicitudes.api.Models
 {
@@ -20,21 +21,27 @@ namespace HorasBeca.Solicitudes.api.Models
         public JObject InsertarSolicitud(JObject data)
         {
             string tipoSolicitud = (string)data.GetValue("tipoSolicitud");
-            data["imgPpg"] = Convert.FromBase64String((string)data.GetValue("imgPpg").ToString());
-            data["imgPps"] = Convert.FromBase64String((string)data.GetValue("imgPps").ToString());
-            data["imgCg"] = Convert.FromBase64String((string)data.GetValue("imgCg").ToString());
-            data["imgCs"] = Convert.FromBase64String((string)data.GetValue("imgCs").ToString());
-            data["imgCBanco"] = Convert.FromBase64String((string)data.GetValue("imgCBanco").ToString());
-            data["imgNotaCurso"] = Convert.FromBase64String((string)data.GetValue("imgNotaCurso").ToString());
+            byte[] imgPpg = Convert.FromBase64String((string)data.GetValue("imgPpg"));
+            data["imgPpg"] = imgPpg;
+            byte[] imgPps = Convert.FromBase64String((string)data.GetValue("imgPps"));
+            data["imgPps"] = imgPps;
+            byte[] imgCg = Convert.FromBase64String((string)data.GetValue("imgCg"));
+            data["imgCg"] = imgCg;
+            byte[] imgCs = Convert.FromBase64String((string)data.GetValue("imgCs"));
+            data["imgCs"] = imgCs;
+            byte[] imgCBanco = Convert.FromBase64String((string)data.GetValue("imgCBanco"));
+            data["imgCBanco"] = imgCBanco;
+            byte[] imgNotaCurso = Convert.FromBase64String((string)data.GetValue("imgNotaCurso"));
+            data["imgNotaCurso"] = imgNotaCurso;
             data.Remove("tipoSolicitud");
             DataTable table = SolicitudWrapper(data);
             JObject respuesta = new JObject();
             SqlConnection dbConexion = new SqlConnection(connectionString);
             dbConexion.Open();
             SqlCommand Comando = new SqlCommand("Solicitud_Test", dbConexion);//LLama un Stored Procedur
-
-            Comando.Parameters.Add("@solicitudWrapper", SqlDbType.Structured).Value = table;
             Comando.CommandType = CommandType.StoredProcedure;
+            Comando.Parameters.Add("@solicitudWrapper", SqlDbType.Structured).Value = table;
+            int temp = Comando.ExecuteNonQuery();
             return respuesta;
         }
 
@@ -42,36 +49,36 @@ namespace HorasBeca.Solicitudes.api.Models
         {
             dynamic temp = data;
             //Se crea la tabla con las columnas
-            DataTable SolicitudWrapper = new DataTable();
-            SolicitudWrapper.Columns.Add("cedula", typeof(int));
-            SolicitudWrapper.Columns.Add("proPonGeneral", typeof(float));
-            SolicitudWrapper.Columns.Add("proPonSemestral", typeof(float));
-            SolicitudWrapper.Columns.Add("creditosGeneral", typeof(int));
-            SolicitudWrapper.Columns.Add("creditosSemestre", typeof(int));
-            SolicitudWrapper.Columns.Add("cuentaBanco", typeof(int));
-            SolicitudWrapper.Columns.Add("banco", typeof(string));
-            SolicitudWrapper.Columns.Add("carne", typeof(string));
-            SolicitudWrapper.Columns.Add("horas", typeof(int));
-            SolicitudWrapper.Columns.Add("imgPpg", typeof(string));
-            SolicitudWrapper.Columns.Add("imgPps", typeof(string));
-            SolicitudWrapper.Columns.Add("imgCg", typeof(string));
-            SolicitudWrapper.Columns.Add("imgCs", typeof(string));
-            SolicitudWrapper.Columns.Add("imgCBanco", typeof(string));
-            SolicitudWrapper.Columns.Add("idCurso", typeof(int));
-            SolicitudWrapper.Columns.Add("idProfesor", typeof(int));
-            SolicitudWrapper.Columns.Add("notaCurso", typeof(int));
-            SolicitudWrapper.Columns.Add("imgNotaCurso", typeof(string));
-            SolicitudWrapper.Columns.Add("fecha", typeof(DateTime));
-            SolicitudWrapper.Columns.Add("telefono", typeof(int));
-            SolicitudWrapper.Columns.Add("anosTEC", typeof(float));
+            DataTable table = new DataTable();
+            table.Columns.Add("cedula", typeof(int));
+            table.Columns.Add("proPonGeneral", typeof(float));
+            table.Columns.Add("proPonSemestral", typeof(float));
+            table.Columns.Add("creditosGeneral", typeof(int));
+            table.Columns.Add("creditosSemestre", typeof(int));
+            table.Columns.Add("cuentaBanco", typeof(int));
+            table.Columns.Add("banco", typeof(string));
+            table.Columns.Add("carne", typeof(string));
+            table.Columns.Add("horas", typeof(int));
+            table.Columns.Add("imgPpg", typeof(SqlBinary));
+            table.Columns.Add("imgPps", typeof(SqlBinary));
+            table.Columns.Add("imgCg", typeof(SqlBinary));
+            table.Columns.Add("imgCs", typeof(SqlBinary));
+            table.Columns.Add("imgCBanco", typeof(SqlBinary));
+            table.Columns.Add("idCurso", typeof(int));
+            table.Columns.Add("idProfesor", typeof(int));
+            table.Columns.Add("notaCurso", typeof(int));
+            table.Columns.Add("imgNotaCurso", typeof(SqlBinary));
+            table.Columns.Add("fecha", typeof(DateTime));
+            table.Columns.Add("telefono", typeof(int));
+            table.Columns.Add("anosTEC", typeof(float));
             //Agregar informacion
-            SolicitudWrapper.Rows.Add((int)temp.cedula, (float)temp.proPonGeneral, (float)temp.proPonSemestral,
+            table.Rows.Add((int)temp.cedula, (float)temp.proPonGeneral, (float)temp.proPonSemestral,
                 (int)temp.creditosGeneral, (int)temp.creditosSemestre, (int)temp.cuentaBanco, (string)temp.banco, 
-                (string)temp.carne, (int)temp.horas, (string)temp.imgPpg, (string)temp.imgPps, (string)temp.imgCg,
-                (string)temp.imgCs, (string)temp.imgCBanco, (int)temp.idCurso, (int)temp.idProfesor, (int)temp.notaCurso,
-                (string)temp.imgNotaCurso, (DateTime)temp.fecha, (int)temp.telefono, (float)temp.anosTEC);
+                (string)temp.carne, (int)temp.horas, (byte[])temp.imgPpg, (byte[])temp.imgPps, (byte[])temp.imgCg,
+                (byte[])temp.imgCs, (byte[])temp.imgCBanco, (int)temp.idCurso, (int)temp.idProfesor, (int)temp.notaCurso,
+                (byte[])temp.imgNotaCurso, (DateTime)temp.fecha, (int)temp.telefono, (float)temp.anosTEC);
 
-            return SolicitudWrapper;
+            return table;
 
         }
 
