3624967830f97370f8f4715306750287002dd715
kevin158 <kevin.gam18@gmail.com>
2018-06-17 03:09:32 -0600

    cambios en vista estudiante Web

--- WEB/Estudiante/studentCtrl.js
+++ WEB/Estudiante/studentCtrl.js
@@ -40,33 +40,104 @@ var app = angular.module("computer", ["ngRoute"])
 	}])
 
 .controller("StudentCtrl",["$scope", "$http",function($scope, $http, $location, MyService){
+	$scope.strCarne = "";
+	$scope.profesores = [{Nombre:"Marco", ID:1},{Nombre:"Isaac", ID:2},{Nombre:"Diego", ID:3}];
+
+	function imgToBase64(file,scp) {
+		if(file!=null){
+			var reader = new FileReader();
+			reader.onload = function(){
+		        //alert(reader.result);
+		        //$scope.strCarne = reader.result;
+		        aisgnarImg(reader.result, scp);
+		    };
+		    reader.readAsDataURL(file);
+		}
+		else{
+			$scope.strCarne = "";
+		}
+	    
+	};
+
+	function aisgnarImg(img,scp) {
+		if(scp==0){
+			$scope.strCarne = img;
+		}
+	};
+
+	function errorImg() {
+		var errImg = false;
+		if($scope.strCarne==""){
+			alert("Debe adjuntar una imagen para carné");
+		}
+		else{
+			errImg = true;
+		}
+		return errImg;
+	};
 
 		$scope.solicitudes = [{"ID":115, "Fecha de Solicitud":"04/02", "Hora de Solicitud":"11:14AM", "Estado":"Pendiente"},{"ID":191, "Fecha de Solicitud":"05/02", "Hora de Solicitud":"08:52AM", "Estado":"Pendiente"}];
+		var bank = "Banco Nacional";
 
-        $scope.hTutor = function(){
-        		window.location.href = "#!hTutor";
+       	$scope.bancoNacional = function(){
+       			bank = document.getElementById('bancos').value;
+        		document.getElementsByName('banco')[0].placeholder='20001';
         	};
 
-        $scope.hAsist = function(){
-        		window.location.href = "#!hAsist";
+        $scope.bancoPopular = function(){
+        		bank = document.getElementById('bancos').value;
+        		document.getElementsByName('banco')[0].placeholder='161';
         	};
-        $scope.hEst = function(){
-        		window.location.href = "#!hEst";
+        $scope.enviarHA = function(){
+        	var fileCarne = document.getElementById('imgCarne').files[0];
+	        imgToBase64(fileCarne,0);
+	        var Data = {};
+	        setTimeout(function(){
+	        		if(errorImg()){
+	        			var localTime = new Date();
+		        		Data = {"cedula":$scope.cedula,
+							"proPonGeneral":$scope.ppg,
+							"proPonSemestral":$scope.pps,
+							"creditosGeneral":$scope.cg,
+							"creditosSemestre":$scope.cs,
+							"cuentaBanco":$scope.bn,
+							"banco":bank,
+							"carne":$scope.carne,
+							"horas":0,
+							"imgPpg":$scope.strCarne,
+							"imgPps":"YW55IGNhcm5hbCBwbGVhc3VyZS4=",
+							"imgCg":"YW55IGNhcm5hbCBwbGVhc3VyZS4=",
+							"imgCs":"YW55IGNhcm5hbCBwbGVhc3VyZS4=",
+							"imgCBanco":"YW55IGNhcm5hbCBwbGVhc3VyZS4=",
+							"imgCed":"YW55IGNhcm5hbCBwbGVhc3VyZS4=",
+							"imgCar":"YW55IGNhcm5hbCBwbGVhc3VyZS4=",
+							"idCurso":parseInt(document.getElementById('curso').value),
+							"idProfesor":parseInt(document.getElementById('profesor').value),
+							"notaCurso":$scope.notaCurso,
+							"imgNotaCurso":"YW55IGNhcm5hbCBwbGVhc3VyZS4=",
+							"fecha":localTime,
+							"telefono":$scope.telefono,
+							"anosTEC":$scope.anTEC,
+							"estado":0,
+							"otrasAsistenciasDesc": ["uno","dos"],
+							"otrasAsistenciasHoras":[5,78]};
+							console.log(Data);alert("Su solicitud ha sido enviada");
+	        		}
+			       
+					
+					/*$http.post("http://192.168.100.7/api/Solicitudes?codigo=S01", Data).
+		        	then(function(response) {
+		            	$scope.res = response.data;
+		            	console.log($scope.res);
+		        	});*/
+					
+			    }, 500);
+        		
+				
+        		
         	};
-        $scope.asistEsp = function(){
-        		window.location.href = "#!asistEsp";
-        	};
-        $scope.enviar = function(){
-        		if (window.File && window.FileReader && window.FileList && window.Blob) {
-        			alert('Todo ak7');
-				  // Great success! All the File APIs are supported.
-				} else {
-				  alert('The File APIs are not fully supported in this browser.');
-				}
-        		alert("Su solicitud ha sido enviada");
-        	};
-        $scope.guardar = function(){
-        		alert("Su solicitud ha sido guardada");
+        $scope.guardarHA = function(){
+	          alert(typeof parseInt(document.getElementById('profesor').value));
         	};
         $scope.cancelar = function(){
         		confirm("¿Desea cancelar la solicitud seleccionada?")
